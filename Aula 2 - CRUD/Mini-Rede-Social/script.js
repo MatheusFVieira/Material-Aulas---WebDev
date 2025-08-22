03d262cdbf64e3c1003985332b494423d884bb0d
MatheusFVieira <matheus.vf06@gmail.com>
2025-08-21 22:41:29 -0300

    Aula_10

--- Aula 2 - CRUD/Mini-Rede-Social/script.js
+++ Aula 2 - CRUD/Mini-Rede-Social/script.js
@@ -15,14 +15,28 @@ let posts = [
     {
         text: "Este é o terceiro post teste",
         category: "Eventos",
+        image: "https://placedog.net/150?random=3",
         date: "12/10/2023 12:00:00"
     }
 ];
 
 window.onload = function(){
+    carregarPosts();
     mostrarPost();
 
     document.querySelector("#postForm").addEventListener("submit", addPost)
+    document.querySelector('#postList').addEventListener('click', handleClick);
+};
+
+function handleClick(infosDoEvento){
+    const action = infosDoEvento.target.dataset.action;
+    const index = infosDoEvento.target.dataset.index;
+
+    if(action === "Editar"){
+        editarPost(index);
+    } else if(action === "Apagar"){
+        apagarPost(index);
+    }
 }
 
 // CREAT
@@ -44,7 +58,8 @@ window.onload = function(){
     posts.unshift(novoPost)
 
 
-    document.querySelector("#postForm").reset()
+    document.querySelector("#postForm").reset();
+    salvarPosts();
     mostrarPost();
  }
 
@@ -55,7 +70,7 @@ function mostrarPost(){
     const listaPosts = document.querySelector("#postList");
     listaPosts.innerHTML = ""
     //Passando pelo array criando um tweet para cada post
-    posts.forEach(pegaItem => {
+    posts.forEach((pegaItem, index) => {
         const cardPost = document.createElement("div")
         cardPost.classList.add("card")
 
@@ -64,8 +79,8 @@ function mostrarPost(){
             <img src="${pegaItem.image}"/>
             <h5>Categoria: ${pegaItem.category}</h5>
             <h6>Data e hora:${pegaItem.date}}</h6>
-            <button>Editar</button>
-            <button>Apagar</button>
+            <button data-action="Editar" data-index="${index}"<i class="fa-solid fa-pen-to-square"></i> Editar</button>
+            <button data-action="Apagar" data-index="${index}"><i class="fa-solid fa-eraser"></i> Apagar</button>
         `
         //Adicionando no HTML
         listaPosts.append(cardPost);
@@ -74,11 +89,36 @@ function mostrarPost(){
 
 //UPDATE
 
-function editarPost(){
+function editarPost(index){
+    const novoTexto = prompt('Edite o conteúdo do post', posts[index].text);
+    posts[index].text = novoTexto;
 
+    salvarPosts();
+    mostrarPost();
 }
 
 //DELETE
-function deletarPost(){
+function apagarPost(index){
+    const confirmar = confirm("Você deseja realmente excluir?");
 
+    if(confirmar){
+        posts.splice(index, 1);
+    }
+    
+    salvarPosts();
+    mostrarPost()
+}
+
+// Local e Section Storage
+
+function salvarPosts(){
+    localStorage.setItem("posts", JSON.stringify(posts));
+}
+
+function carregarPosts(){
+    const postsGuardados = localStorage.getItem("posts")
+
+    if(postsGuardados){
+        posts = JSON.parse(postsGuardados);
+    }
 }
