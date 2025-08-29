afad36f820c5b2e0ac4b7ba1d9dd210f6b120d64
MatheusFVieira <matheus.vf06@gmail.com>
2025-08-28 22:40:41 -0300

    Aula_4

---
+++ Aula_4/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import Header from './components/Header'
+
+function App() {
+
+  return (
+    <>
+    <Header 
+      titulo="Matheus"
+      texto="Texto 1"/>
+    <Header 
+      titulo="Lívia"
+      texto="Texto 2"/>
+    <Header 
+      titulo="Tom"
+      texto="Texto 3"/>
+    </>
+  )
+}
+
+export default App
---
+++ Aula_4/src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header(props){
+    return(
+        <>
+            <h1>{props.titulo}</h1>
+            <p>{props.texto}</p>
+        </>        
+    )
+}
+
+export default Header
