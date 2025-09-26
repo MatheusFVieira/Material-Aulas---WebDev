48b4d482a91e86fb3f1eb90df78e5135596f850e
MatheuFVieira <matheus.vf06@gmail.com>
2025-09-25 22:31:41 -0300

    Aula_7

--- Aula 6 - APIs, Hooks e Deploy/react-dashboard/src/pages/Relatorios.jsx
+++ Aula 6 - APIs, Hooks e Deploy/react-dashboard/src/pages/Relatorios.jsx
@@ -1,8 +1,24 @@
+import { GraficoBarras } from "@/components/GraficoBarras";
+import { GraficoDeitado } from "@/components/GraficoDeitado";
+import { Button } from "@/components/ui/button"
+
 export default function Relatorios() {
   return (
-    <div className="p-6 flex-1 bg-gray-100">
-      <h1 className="text-2xl font-bold">Relatórios</h1>
-      <p className="mt-2 text-gray-600">Página de relatórios.</p>
+    <div>
+      <div className="p-6 flex-1 bg-gray-100">
+        <h1 className="text-2xl font-bold">Relatórios</h1>
+        <p className="mt-2 text-gray-600">Página de relatórios.</p>
+        <br/>
+        <Button>Click me</Button>
+      </div>
+      <div className="grid grid-cols-2 gap-4">
+        <div>
+          <GraficoBarras />
+        </div>
+        <div>
+          <GraficoDeitado/>
+        </div>
+      </div>
     </div>
   );
 }
